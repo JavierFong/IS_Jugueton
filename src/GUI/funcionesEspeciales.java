6889b5c344da9092a851420d60cf01bd5292dabb
diegojacobs <diegojacobs@users.noreply.github.com>
2015-07-29 17:08:57 -0600

    Update funcionesEspeciales.java

--- src/GUI/funcionesEspeciales.java
+++ src/GUI/funcionesEspeciales.java
@@ -8,7 +8,7 @@ import backEnd.*;
 import javax.swing.JOptionPane;
 /**
  *
- * @author Work
+ * @author Diego Jacobs 13160
  */
 public class funcionesEspeciales extends javax.swing.JFrame {
     AdministradorJuguetes jAdmin = new AdministradorJuguetes();
@@ -154,6 +154,88 @@ public class funcionesEspeciales extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        Proveedor[] listado = pAdmin.mayoresProveedores();
+        listado = pAdmin.sort(listado);
+        int lim = listado.length; 
+        Object[][] data = new Object[lim][3];
+        for (int i = 0; i<lim; i++) {
+            data[i][0] = listado[i].getCodigo();
+            data[i][1] = listado[i].getNombre(); 
+            data[i][2] = listado[i].getCant_juguetes(); 
+            
+        }
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            data,
+            new String [] {
+                "Codigo", "Nombre", "Juguetes"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        Proveedor[] list = pAdmin.getListado(); 
+        String[] nombres = new String[list.length]; 
+        for (int i=0; i<list.length; i++) {
+            nombres[i] = list[i].getNombre(); 
+        }
+        String prov = "";
+        prov = (String) JOptionPane.showInputDialog(this, "Proveedor:", "", JOptionPane.QUESTION_MESSAGE,null,nombres,nombres[0]);
+        int codigo = pAdmin.getCodigo(prov);
+        Proveedor prv = pAdmin.getProveedor(codigo); 
+        JOptionPane.showMessageDialog(this, "El Proveedor "+prv.getNombre()+" con codigo "+prv.getCodigo()+",\nactualmente nos provee "+prv.getCant_juguetes()+" juguete(s)");
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        Juguete jgt = jAdmin.masCaro(); 
+        JOptionPane.showMessageDialog(this,"El juguete mas caro es el del codigo "+jgt.getCodigo()+",\ncon un valor real de: "+jgt.getValorReal()); 
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+    
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+        Juguete[] listado = jAdmin.juguetesMecanicos();
+        int lim = listado.length; 
+        Object[][] data = new Object[lim][10];
+        for (int i = 0; i<lim; i++) {
+            data[i][0] = listado[i].getCodigo();
+            data[i][1] = listado[i].getCodProveedor(); 
+            data[i][2] = listado[i].getMarca();
+            data[i][3] = listado[i].getEdad(); 
+            data[i][4] = listado[i].getTipo();
+            data[i][5] = listado[i].getComplejidad(); 
+            data[i][6] = listado[i].getValorUnitario(); 
+            data[i][7] = listado[i].getValorAdicional(); 
+            data[i][8] = listado[i].getValorReal(); 
+            data[i][9] = listado[i].getCantidad(); 
+            
+        }
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            data,
+            new String [] {
+                "Codigo", "Proovedor", "Marca", "Edad", "Tipo", "Complejidad", "Valor Uni.", "Valor Ad.","Valor real", "Cantidad"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false,false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
     private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
         // TODO add your handling code here:
         Proveedor[] list = pAdmin.getListado(); 
@@ -196,94 +278,6 @@ public class funcionesEspeciales extends javax.swing.JFrame {
         });
     }//GEN-LAST:event_jMenuItem6ActionPerformed
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        // TODO add your handling code here:
-        Proveedor[] listado = pAdmin.mayoresProveedores();
-        listado = pAdmin.sort(listado);
-        int lim = listado.length; 
-        Object[][] data = new Object[lim][3];
-        for (int i = 0; i<lim; i++) {
-            data[i][0] = listado[i].getCodigo();
-            data[i][1] = listado[i].getNombre(); 
-            data[i][2] = listado[i].getCant_juguetes(); 
-            
-        }
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            data,
-            new String [] {
-                "Codigo", "Nombre", "Juguetes"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                false, false, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
-
-    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
-        // TODO add your handling code here:
-        menuInicio mi = new menuInicio(getActual()); 
-        mi.setVisible(true); 
-    }//GEN-LAST:event_formWindowClosed
-
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
-        Proveedor[] list = pAdmin.getListado(); 
-        String[] nombres = new String[list.length]; 
-        for (int i=0; i<list.length; i++) {
-            nombres[i] = list[i].getNombre(); 
-        }
-        String prov = "";
-        prov = (String) JOptionPane.showInputDialog(this, "Proveedor:", "", JOptionPane.QUESTION_MESSAGE,null,nombres,nombres[0]);
-        int codigo = pAdmin.getCodigo(prov);
-        Proveedor prv = pAdmin.getProveedor(codigo); 
-        JOptionPane.showMessageDialog(this, "El Proveedor "+prv.getNombre()+" con codigo "+prv.getCodigo()+",\nactualmente nos provee "+prv.getCant_juguetes()+" juguete(s)");
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
-
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        // TODO add your handling code here:
-        Juguete jgt = jAdmin.masCaro(); 
-        JOptionPane.showMessageDialog(this,"El juguete mas caro es el del codigo "+jgt.getCodigo()+",\ncon un valor real de: "+jgt.getValorReal()); 
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
-
-    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        // TODO add your handling code here:
-        Juguete[] listado = jAdmin.juguetesMecanicos();
-        int lim = listado.length; 
-        Object[][] data = new Object[lim][10];
-        for (int i = 0; i<lim; i++) {
-            data[i][0] = listado[i].getCodigo();
-            data[i][1] = listado[i].getCodProveedor(); 
-            data[i][2] = listado[i].getMarca();
-            data[i][3] = listado[i].getEdad(); 
-            data[i][4] = listado[i].getTipo();
-            data[i][5] = listado[i].getComplejidad(); 
-            data[i][6] = listado[i].getValorUnitario(); 
-            data[i][7] = listado[i].getValorAdicional(); 
-            data[i][8] = listado[i].getValorReal(); 
-            data[i][9] = listado[i].getCantidad(); 
-            
-        }
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            data,
-            new String [] {
-                "Codigo", "Proovedor", "Marca", "Edad", "Tipo", "Complejidad", "Valor Uni.", "Valor Ad.","Valor real", "Cantidad"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false,false, false, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-    }//GEN-LAST:event_jMenuItem4ActionPerformed
-
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
         // TODO add your handling code here:
         Juguete[] listado = jAdmin.juguetesElectronicos();
@@ -317,7 +311,7 @@ public class funcionesEspeciales extends javax.swing.JFrame {
             }
         });
     }//GEN-LAST:event_jMenuItem5ActionPerformed
-
+    
     private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
         // TODO add your handling code here:
         String[] tipos = new String[] {"Mecanico","Electronico"};
@@ -332,7 +326,13 @@ public class funcionesEspeciales extends javax.swing.JFrame {
             listado = jAdmin.juguetesElectronicos();
             listado = jAdmin.sort(listado);
         }
-    }//GEN-LAST:event_jMenuItem7ActionPerformed
+    }//GEN-LAST:event_jMenuItem7ActionPerformed    
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        // TODO add your handling code here:
+        menuInicio mi = new menuInicio(getActual()); 
+        mi.setVisible(true); 
+    }//GEN-LAST:event_formWindowClosed
 
     /**
      * @param args the command line arguments
